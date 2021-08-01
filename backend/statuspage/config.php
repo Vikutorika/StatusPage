d6597bda4fdb176454d965548afa0d1ec2e87aec
Pesy Wu <admin@bili33.top>
2021-08-01 16:55:16 +0800

    Update config.php

--- backend/statuspage/config.php
+++ backend/statuspage/config.php
@@ -2,7 +2,7 @@
 namespace NerdBaggy\StatusPage;
 
 //Uptime Robot API Key
-define('apiKey', 'Your-API-Key-Here');
+define('apiKey', 'u903555-a41d2e4ebd886b27d8541a80');
 
 //Uptime percentage for following days
 define('historyDay', '1-7-30-360');
